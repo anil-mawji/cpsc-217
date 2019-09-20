d90f96eb45d66284b542ffb5ca745f8c19bab393
Anil Mawji <mawjianil1@gmail.com>
2019-09-19 19:56:32 -0600

    New list printing method

--- Exercise5.py
+++ Exercise5.py
@@ -3,4 +3,5 @@ while value != " ":
     if value is not None:
         tab.append(value)
     value = input("Enter a value (blank line to quit): ")
+# Display list in reverse order
 print(*reversed(tab), sep="\n")
