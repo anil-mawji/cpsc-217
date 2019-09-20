e8794b868ec99f1b3fcd790f665a2861a053f176
Anil Mawji <mawjianil1@gmail.com>
2019-09-19 22:53:44 -0600

    Improved input formatting

--- Exercise5.py
+++ Exercise5.py
@@ -2,6 +2,6 @@ value, tab = None, []
 while value != " ":
     if value is not None:
         tab.append(value)
-    value = input("Enter a value (blank line to quit): ")
+    value = input("Enter a value (blank line to quit):\n")
 # Display list in reverse order
 print(*reversed(tab), sep="\n")
