6b64f741772539ffe7cc0b4146aa3bd0b03a4507
Anil Mawji <mawjianil1@gmail.com>
2019-09-21 11:48:41 -0600

    Created function for getting dog years

--- Exercise3.py
+++ Exercise3.py
@@ -2,25 +2,35 @@
 # CPSC 217 Exercise 3: Better Human Years and Dog Years
 # Copyright (C) 2019 Anil Mawji
 
+
+# Convert a given age to an age in dog years
+# @param an age in human years
+# @return an age in dog years
+def get_dog_years(years):
+    dog_years = 0
+    if years <= 2:
+        # First two dog years are equivalent to 10.5 human years
+        return years * 10.5
+    # Add 10.5 years to dog_years a maximum of two times
+    for i in range(2):
+        if years - 1 >= 0:
+            dog_years = dog_years + 10.5
+            years = years - 1
+        else:
+            # Age is age is too small to keep adding
+            break
+    # Add 4 years for every remaining year
+    return dog_years + years * 4
+
+
 while True:
     try:
         age = float(input("Enter your age:\n"))
-        dogAge = 0
-        # Add 10.5 years to dogAge a maximum of two times
-        for i in range(2):
-            if age - 1 >= 0:
-                dogAge = dogAge + 10.5
-                age = age - 1
-            else:
-                # Age is too small to keep adding
-                break
-        # Add 4 years for every year remaining in age
-        dogAge = dogAge + age*4
         if age < 0:
             # Exit program
             break
-        # Display result
-        print("That's equivalent to", dogAge, "dog years!")
+        # Round the result to 1 decimal place
+        print("That's equivalent to", "%.1f" % get_dog_years(age), "dog years!")
     except ValueError:
-        # If input is not a number, ask for age again
+        # If input was not a number then ask for age again
         pass
