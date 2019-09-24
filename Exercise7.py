6848cb77482b9b7bb61876254b24a22e2a5c937c
anil.mawji <mawjianil1@gmail.com>
2019-09-24 13:26:21 -0600

    Improve formatting

--- Exercise7.py
+++ Exercise7.py
@@ -2,7 +2,7 @@
 # CPSC 217 Exercise 7: Counting... 1... 2... 3...
 # Copyright (C) 2019 Anil Mawji
 
-name = input("Enter the name of the text file:\n")
-with open(name + ".txt", "r") as f:
-    print(sum(line.count(",") for line in f))
+name = input("Enter the name of a file:\n")
+with open(name, "r") as f:
+    print("That file contains", sum(line.count(",") for line in f), "values.")
 f.close()
