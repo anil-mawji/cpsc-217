ff7a513fbff796255590705f96f21c7e3984b988
anil.mawji <mawjianil1@gmail.com>
2019-09-25 10:44:54 -0600

    Remove name var

--- Exercise7.py
+++ Exercise7.py
@@ -2,7 +2,6 @@
 # CPSC 217 Exercise 7: Counting... 1... 2... 3...
 # Copyright (C) 2019 Anil Mawji
 
-name = input("Enter the name of a file:\n")
-with open(name, "r") as f:
+with open(input("Enter the name of a file:\n"), "r") as f:
     print("That file contains", sum(line.count(",") for line in f), "values.")
 f.close()
