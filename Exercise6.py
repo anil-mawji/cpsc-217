8418a2985400ffb80b6658ff2d47caa3d86b4c9f
anil.mawji <mawjianil1@gmail.com>
2019-09-25 10:36:24 -0600

    First working version

--- Exercise6.py
+++ Exercise6.py
@@ -1,18 +1,23 @@
 PROVINCE_LETTERS = {
-    "Alberta": "T",
-    "British Columbia": "V",
-    "Manitoba": "R",
-    "New Brunswick": "E",
-    "Newfoundland": "A",
-    "Nova Scotia": "B",
-    "Nunavut": "X",
-    "Northwest Territories": "X",
-    "Ontario": ["K", "L", "M", "N", "P"],
-    "Prince Edward Island": "C",
-    "Quebec": ["G", "H", "J"],
-    "Saskatchewan": "S",
-    "Yukon": "Y"
+    "T": "Alberta",
+    "V": "British Columbia",
+    "R": "Manitoba",
+    "E": "New Brunsiwick",
+    "A": "Newfoundland",
+    "B": "Nova Scotia",
+    "X": "Nunavut or Northwest Territories",
+    "K": "Ontario",
+    "L": "Ontario",
+    "M": "Ontario",
+    "N": "Ontario",
+    "P": "Ontario",
+    "C": "Prince Edward Island",
+    "G": "Quebec",
+    "H": "Quebec",
+    "J": "Quebec",
+    "S": "Saskatchewan",
+    "Y": "Yukon"
 }
 
-# postal_code = input("Enter a 6 character postal code (A1A1A1): ")
-# print("That postal code resides in")
+postalCode = input("Enter a 6 character postal code (A1A1A1): ")
+print("That postal code resides in", PROVINCE_LETTERS[postalCode[0]])
