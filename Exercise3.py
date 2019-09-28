66fb9a56d4ac4265a5985075e630180cc2781d0a
Anil Mawji <mawjianil1@gmail.com>
2019-09-28 12:55:37 -0600

    Add inline if

--- Exercise3.py
+++ Exercise3.py
@@ -2,22 +2,15 @@
 # CPSC 217 Exercise 3: Better Human Years and Dog Years
 # Copyright (C) 2019 Anil Mawji
 
+HUMAN_TO_DOG_YEARS = 10.5
+
 
 # Convert a given age to an age in dog years
 # @param an age in human years
 # @return an age in dog years
 def get_dog_years(years):
-    if years <= 2:
-        # First two dog years are equivalent to 10.5 human years
-        return years * 10.5
-    dog_years = 0
-    # Add 10.5 years to dog_years a maximum of two times
-    for _ in range(2):
-        if years - 1 >= 0:
-            dog_years = dog_years + 10.5
-            years = years - 1
-    # Add 4 years for every remaining year
-    return dog_years + years * 4
+    # First two dog years are equivalent to 10.5 human years
+    return years * HUMAN_TO_DOG_YEARS if years <= 2 else (years - 2) * 4 + HUMAN_TO_DOG_YEARS * 2
 
 
 while True:
@@ -27,7 +20,7 @@ while True:
             # Exit program
             break
         # Round the result to 1 decimal place
-        print("That's equivalent to", "%.1f" % get_dog_years(age), "dog years!")
+        print("That's equivalent to %.1f" % get_dog_years(age), "dog years!")
     except ValueError:
         # If input was not a number then ask for age again
         pass
