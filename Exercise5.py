809338a3a14a3e1f1208c88148c7daf1db4a92ab
anil.mawji <mawjianil1@gmail.com>
2019-09-30 11:35:48 -0600

    Break loop with enter key

--- Exercise5.py
+++ Exercise5.py
@@ -1,11 +1,16 @@
 ##
 # CPSC 217 Exercise 5: Forward and Backward
 # Copyright (C) 2019 Anil Mawji
+# UCID: 30099809
+
 
 value, tab = None, []
-while value != " ":
-    if value is not None:
-        tab.append(value)
+while True:
     value = input("Enter a value (blank line to quit): ")
+    # If user did not enter input, break loop
+    if len(value) == 0:
+        break
+    # Add value to the list
+    tab.append(value)
 # Display list in reverse order
 print(*reversed(tab), sep="\n")
