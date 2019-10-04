fd69982550ae236911fb004e6a030d73dca5dc1f
anil.mawji <mawjianil1@gmail.com>
2019-10-04 11:35:26 -0600

    Fix axis drawing method

--- Assignment2.py
+++ Assignment2.py
@@ -8,20 +8,24 @@ from SimpleGraphics import *
 WIDTH = 800
 HEIGHT = 600
 
+background("white")
 
 # Draw x-axis
 line(0, HEIGHT / 2, WIDTH, HEIGHT / 2)
-
-for i in range(int(WIDTH / 2), 0, -30):
-    line(i, HEIGHT / 2 - 3, i, HEIGHT / 2 + 3)
-
-for i in range(int(WIDTH / 2), WIDTH, 30):
+for i in range(-5, WIDTH, 30):
     line(i, HEIGHT / 2 - 3, i, HEIGHT / 2 + 3)
 
 # Draw y-axis
 line(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
-
 for i in range(0, HEIGHT, 30):
     line(WIDTH / 2 - 3, i, WIDTH / 2 + 3, i)
 
-equation = input("Enter an equation of x as a function of y: ")
+expression = input("Enter an arithmetic expression: ")
+last = None
+x = 0
+while x < 26 * 30:
+    result = eval("math.cos(%f)" % x) * 30
+    print("When x is", x, "the value of the expression is", result)
+    line((*last, x, result) if last else (0, 0, x, result))
+    last = (x, result)
+    x += 30
