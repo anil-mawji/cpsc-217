b699eba23aac7830e0a8dfe593968fdc6086d577
anil.mawji <mawjianil1@gmail.com>
2019-10-07 18:02:55 -0600

    First working version

--- Assignment2.py
+++ Assignment2.py
@@ -4,29 +4,49 @@
 # UCID: 30099809
 
 from SimpleGraphics import *
+from math import *
 
 WIDTH = 800
 HEIGHT = 600
+origin = {"x": WIDTH / 2, "y": HEIGHT / 2}
 
 background("white")
 
 # Draw x-axis
-line(0, HEIGHT / 2, WIDTH, HEIGHT / 2)
-for i in range(-5, WIDTH, 30):
-    line(i, HEIGHT / 2 - 3, i, HEIGHT / 2 + 3)
+line(0, origin["y"], WIDTH, origin["y"])
+for x in range(10, WIDTH, 30):
+    # Draw horizontal tick
+    line(x, origin["y"] - 3, x, origin["y"] + 3)
+    label = x // 30 - 13
+    if label != 0:
+        # Draw number label
+        text(x, origin["y"] + 10, label)
 
 # Draw y-axis
-line(WIDTH / 2, 0, WIDTH / 2, HEIGHT)
-for i in range(0, HEIGHT, 30):
-    line(WIDTH / 2 - 3, i, WIDTH / 2 + 3, i)
+line(origin["x"], 0, origin["x"], HEIGHT)
+for y in range(-HEIGHT, 30, 30):
+    # Draw vertical tick
+    line(origin["x"] - 3, -y, origin["x"] + 3, -y)
+    label = y // 30 + 10
+    if label != 0:
+        # Draw number label
+        text(origin["x"] - 10, -y, label)
 
-expression = input("Enter an arithmetic expression: ")
-last = None
-x = 0
-while x < 26 * 30:
-    result = eval("math.cos(%f)" % x) * 30
-    print("When x is", x, "the value of the expression is", result)
-    line((*last, x, result) if last else (0, 0, x, result))
-    last = (x, result)
-    x += 30
+expr = "x"
+color = None
 
+while len(expr) > 0:
+    expr = input("Enter an arithmetic expression: ")
+
+    if len(expr) > 0:
+        # Set graph color
+        color = "red" if not color else "blue" if color == "red" else "green" if color == "blue" else "red"
+        setColor(color)
+
+        # Plot points
+        x = -13
+        while x < 13:
+            y = eval(expr)
+            rect(origin["x"] + x * 30, origin["y"] - y * 30, 5, 5)
+            print("y = {}".format(y))
+            x += 0.1
