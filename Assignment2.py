a76a4772704b0d58cd6111d034893545df2c0250
anil.mawji <mawjianil1@gmail.com>
2019-10-21 10:39:59 -0600

    Plot local max & min points

--- Assignment2.py
+++ Assignment2.py
@@ -12,16 +12,17 @@ HEIGHT = 600
 # Size of the tick mark on the axes
 TICK_SIZE = 6
 # Num pixels per unit
-UNIT_SIZE = 30
+SCALE = 30
 # Colors to use when drawing the graph
 COLORS = ["red", "blue", "green"]
 
 # Location of the origin (0, 0) on the screen
-ORIGIN = {"x": WIDTH / 2, "y": HEIGHT / 2}
+ORIGIN_X = WIDTH // 2
+ORIGIN_Y = HEIGHT // 2
 # Num units in the positive/negative x-axis
-UNITS_X = WIDTH // UNIT_SIZE // 2
+UNITS_X = WIDTH // SCALE // 2
 # Num units in the positive/negative y-axis
-UNITS_Y = HEIGHT // UNIT_SIZE // 2
+UNITS_Y = HEIGHT // SCALE // 2
 # How much x increases between each calculation of y
 DELTA_X = 0.2
 
@@ -31,55 +32,87 @@ color_index = 0
 background("white")
 
 # Draw x-axis
-line(0, ORIGIN["y"], WIDTH, ORIGIN["y"])
-for x in range(10, WIDTH, UNIT_SIZE):
+line(0, ORIGIN_Y, WIDTH, ORIGIN_Y)
+for x in range(10, WIDTH, SCALE):
     # Draw horizontal tick 6 pixels tall
-    line(x, ORIGIN["y"] - TICK_SIZE / 2, x, ORIGIN["y"] + TICK_SIZE / 2)
+    line(x, ORIGIN_Y - TICK_SIZE / 2, x, ORIGIN_Y + TICK_SIZE / 2)
 
-    label = x // UNIT_SIZE - UNITS_X
+    label = x // SCALE - UNITS_X
     if label != 0:
         # Draw number label
-        text(x, ORIGIN["y"] + UNITS_Y + TICK_SIZE / 2, label)
+        text(x, ORIGIN_Y + UNITS_Y + TICK_SIZE / 2, label)
 
 # Draw y-axis
-line(ORIGIN["x"], 0, ORIGIN["x"], HEIGHT)
-for y in range(-HEIGHT, UNIT_SIZE, UNIT_SIZE):
+line(ORIGIN_X, 0, ORIGIN_X, HEIGHT)
+for y in range(-HEIGHT, SCALE, SCALE):
     # Draw vertical tick 6 pixels wide
-    line(ORIGIN["x"] - TICK_SIZE / 2, -y, ORIGIN["x"] + TICK_SIZE / 2, -y)
+    line(ORIGIN_X - TICK_SIZE / 2, -y, ORIGIN_X + TICK_SIZE / 2, -y)
 
-    label = y // UNIT_SIZE + UNITS_Y
+    label = y // SCALE + UNITS_Y
     if label != 0:
         # Draw number label
-        text(ORIGIN["x"] - UNITS_Y - TICK_SIZE / 2, -y, label)
+        text(ORIGIN_X - UNITS_Y - TICK_SIZE / 2, -y, label)
 
 
 # Converts x and y values to their corresponding position on the screen
-# @px an x value on a graph
-# @py the y value calculated from the expression
-# @return the position of px and py on the screen
+# @param px     an x value on a graph
+# @param py     the y value calculated from the expression
+# @param return the position of px and py on the screen
 def get_screen_coordinates(px, py):
-    return ORIGIN["x"] + px * UNIT_SIZE, ORIGIN["y"] - py * UNIT_SIZE
+    return ORIGIN_X + px * SCALE, ORIGIN_Y - py * SCALE
 
 
-while len(expr) > 0:
+# Draws a curve to the screen
+# @param equation of the function to be drawn
+def draw_function(expression):
+    # Set initial x value at the left side of x-axis
+    x = -UNITS_X
+    # Stores the x and y values of the previously calculated point
+    last_pt = x, eval(expression)
+    # Plot points
+
+    while x < UNITS_X:
+        current_pt = x, eval(expression)
+        # Unpack the coordinates of the last and the new point
+        # Draw a line from the last point to the new point
+        line(*get_screen_coordinates(*last_pt),
+             *get_screen_coordinates(*current_pt))
+
+        x += DELTA_X
+        next_pt = x, eval(expression)
+        draw_extrema(last_pt, current_pt, next_pt)
+        last_pt = current_pt
+
+
+# Checks if a point is a max or min based on the points before and after it
+# @param last_point    the point on the graph that comes prior to current_point
+# @param current_point the current point being checked
+# @param next_point    the point on the graph that follows current_point
+def draw_extrema(last_point, current_point, next_point):
+    slope_last = (current_point[1] - last_point[1]) / DELTA_X
+    slope_next = (next_point[1] - current_point[1]) / DELTA_X
+
+    # Local minimum
+    if slope_last < 0 < slope_next:
+        setColor("purple")
+        ellipse(*get_screen_coordinates(current_point[0] - DELTA_X / 2, current_point[1]), 5, 5)
+    # Local maximum
+    elif slope_last > 0 > slope_next:
+        setColor("orange")
+        ellipse(*get_screen_coordinates(current_point[0] - DELTA_X / 2, current_point[1]), 5, 5)
+    # Reset color setting
+    setColor(COLORS[color_index])
+
+
+while expr != "":
     expr = input("Enter the expression (blank line to quit):\ny = ")
-    if len(expr) == 0:
+    if expr == "":
         close()
         exit(0)
 
     # Set graph color
     setColor(COLORS[color_index])
+    # Sketch curve
+    draw_function(expr)
+    # Update graph color
     color_index = color_index + 1 if color_index + 1 < len(COLORS) else 0
-
-    # Set initial x value at the left side of x-axis
-    x = -UNITS_X
-    # Stores the x and y values of the previously calculated point
-    last = get_screen_coordinates(x, eval(expr))
-    # Plot points
-    while x < UNITS_X:
-        y = eval(expr)
-        # Unpack the coordinates of the last and the new point
-        # Draw a line from the last point to the new point
-        line(*last, *get_screen_coordinates(x, y))
-        last = get_screen_coordinates(x, y)
-        x += DELTA_X
