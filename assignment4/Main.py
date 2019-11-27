d0158c13dd402892836612d5944d5e7be066bcb2
anil.mawji <mawjianil1@gmail.com>
2019-11-27 11:32:10 -0700

    Draw polygons & colors

--- assignment4/Main.py
+++ assignment4/Main.py
@@ -9,10 +9,21 @@ from SimpleGraphics import *
 
 WIDTH = getWidth()
 HEIGHT = getHeight()
+
 PADDING_X = 150
 PADDING_Y = 75
+
+SPACING_X = 10
+SPACING_Y = 10
+
 BAR_WIDTH = 25
-SPACING = 10
+
+# Colors are taken from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+COLORS = [
+    (230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200), (245, 130, 48), (145, 30, 180), (70, 240, 240),
+    (240, 50, 230), (210, 245, 60), (250, 190, 190), (0, 128, 128), (230, 190, 255), (170, 110, 40), (255, 250, 200),
+    (128, 0, 0), (170, 255, 195), (128, 128, 0), (255, 215, 180), (0, 0, 128), (128, 128, 128), (255, 255, 255)
+]
 
 
 # Draws a Sankey diagram of a given data set
@@ -22,19 +33,42 @@ SPACING = 10
 def draw_sankey(data):
     # Calculate bar scale
     total_flow = sum(float(flow) for flow in data.values())
-    num_pixels = HEIGHT - PADDING_Y * 2 - (len(data) - 1) * SPACING
+    num_pixels = HEIGHT - PADDING_Y * 2 - (len(data) - 1) * SPACING_Y
     pixels_per_unit = num_pixels / total_flow
 
     # Draw source bar
-    rect(130, (HEIGHT - total_flow * pixels_per_unit) / 2, BAR_WIDTH, total_flow * pixels_per_unit)
+    source_x = PADDING_X + SPACING_X
+    source_y = (HEIGHT - total_flow * pixels_per_unit) / 2
+    setColor(*COLORS[0])
+    rect(source_x, source_y, BAR_WIDTH, total_flow * pixels_per_unit)
 
     # Draw destination rectangles
-    offset = 0
+    destination_x = WIDTH - PADDING_X
+    destination_y = PADDING_Y
     for k in data:
         height = float(data[k]) * pixels_per_unit
-        rect(WIDTH - PADDING_X, PADDING_Y + offset, BAR_WIDTH, height)
-        text(WIDTH - PADDING_X + BAR_WIDTH, PADDING_Y + offset + height / 2, k)
-        offset += height + SPACING
+
+        # Draw destination bar
+        setColor(*COLORS[list(data).index(k) + 1])
+        rect(destination_x, destination_y, BAR_WIDTH, height)
+
+        # Draw destination text
+        setColor("black")
+        text(destination_x + BAR_WIDTH + SPACING_X, PADDING_Y + destination_y + height / 2, k, "w")
+
+        # Draw destination body
+        polygon(
+            source_x + BAR_WIDTH,  # X1
+            source_y,  # Y1
+            destination_x,  # X2
+            destination_y,  # Y2
+            destination_x,  # X3
+            destination_y + height,  # Y3
+            source_x + BAR_WIDTH,  # X4
+            source_y + height  # Y4
+        )
+        source_y += height
+        destination_y += height + SPACING_Y
 
 
 #
@@ -49,8 +83,8 @@ def collect_data(file):
             setFont("Calibri", "20")
             text(WIDTH / 2, 50, line)
         elif i == 1:
-            setFont("Calibri", "10")
-            text(100, HEIGHT / 2, line)
+            setFont("Calibri")
+            text(PADDING_X, HEIGHT / 2, line, "e")
         else:
             line = line.split(", ")
             data[line[0]] = float(line[1])
