d139991b1fac9c91afa52c84ee10f9e8cf432d22
Anil Mawji <mawjianil1@gmail.com>
2019-11-27 19:36:04 -0700

    Add color gradient

--- assignment4/Main.py
+++ assignment4/Main.py
@@ -5,19 +5,19 @@
 #
 # Program description:
 
+import sys
+import math
 from SimpleGraphics import *
+
+sys.argv = ['Main.py', 'Baseball.txt']
 
 WIDTH = getWidth()
 HEIGHT = getHeight()
-
 PADDING_X = 150
 PADDING_Y = 75
-
 SPACING_X = 10
 SPACING_Y = 10
-
 BAR_WIDTH = 25
-
 # Colors are taken from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
 COLORS = [
     (230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200), (245, 130, 48), (145, 30, 180), (70, 240, 240),
@@ -32,7 +32,7 @@ COLORS = [
 # @return
 def draw_sankey(data):
     # Calculate bar scale
-    total_flow = sum(float(flow) for flow in data.values())
+    total_flow = sum(flow for flow in data.values())
     num_pixels = HEIGHT - PADDING_Y * 2 - (len(data) - 1) * SPACING_Y
     pixels_per_unit = num_pixels / total_flow
 
@@ -42,31 +42,33 @@ def draw_sankey(data):
     setColor(*COLORS[0])
     rect(source_x, source_y, BAR_WIDTH, total_flow * pixels_per_unit)
 
-    # Draw destination rectangles
     destination_x = WIDTH - PADDING_X
     destination_y = PADDING_Y
-    for k in data:
-        height = float(data[k]) * pixels_per_unit
 
-        # Draw destination bar
-        setColor(*COLORS[list(data).index(k) + 1])
-        rect(destination_x, destination_y, BAR_WIDTH, height)
+    for k in data:
+        color = COLORS[list(data).index(k) + 1]
+        height = data[k] * pixels_per_unit
 
         # Draw destination text
         setColor("black")
-        text(destination_x + BAR_WIDTH + SPACING_X, PADDING_Y + destination_y + height / 2, k, "w")
+        text(destination_x + BAR_WIDTH + SPACING_X, destination_y + height / 2, k, "w")
 
-        # Draw destination body
-        polygon(
-            source_x + BAR_WIDTH,  # X1
-            source_y,  # Y1
-            destination_x,  # X2
-            destination_y,  # Y2
-            destination_x,  # X3
-            destination_y + height,  # Y3
-            source_x + BAR_WIDTH,  # X4
-            source_y + height  # Y4
-        )
+        # Draw destination bar
+        setColor(*color)
+        rect(destination_x, destination_y, BAR_WIDTH, height)
+
+        # Draw body
+        offset_y = 0
+        increment = (source_y - destination_y) / (destination_x - source_x - BAR_WIDTH)
+        range_x = destination_x - source_x - BAR_WIDTH
+        for x in range(range_x):
+            setColor(x / range_x * color[0],
+                     x / range_x * color[1],
+                     x / range_x * color[2])
+            line(source_x + BAR_WIDTH + x, source_y - offset_y,
+                 source_x + BAR_WIDTH + x, source_y + height - offset_y)
+            offset_y += increment
+
         source_y += height
         destination_y += height + SPACING_Y
 
@@ -76,18 +78,19 @@ def draw_sankey(data):
 # @param file
 # @return data
 def collect_data(file):
+    setColor("black")
+    setFont("Calibri", "20")
+    text(WIDTH / 2, 50, file.readline())
+
+    setFont("Calibri")
+    text(PADDING_X, HEIGHT / 2, file.readline(), "e")
+
     data = {}
-    for i, line in enumerate(file):
-        if i == 0:
-            setColor("black")
-            setFont("Calibri", "20")
-            text(WIDTH / 2, 50, line)
-        elif i == 1:
-            setFont("Calibri")
-            text(PADDING_X, HEIGHT / 2, line, "e")
-        else:
-            line = line.split(", ")
-            data[line[0]] = float(line[1])
+    ln = file.readline()
+    while ln:
+        line_info = ln.split(",")
+        data[line_info[0]] = float(line_info[1])
+        ln = file.readline()
     return data
 
 
@@ -95,11 +98,9 @@ def main():
     setWindowTitle("Assignment 4")
     background("light gray")
 
-    with open("Baseball.txt") as file:
-    # with open(input("Enter the name of the file: ") + ".txt") as file:
-        data = collect_data(file)
+    with open(sys.argv[1]) as file:
+        draw_sankey(collect_data(file))
         file.close()
-    draw_sankey(data)
 
 
 if __name__ == '__main__':
