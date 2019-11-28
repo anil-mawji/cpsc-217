65b4304d813e09490b81c36d9f38da145b3d270a
Anil Mawji <mawjianil1@gmail.com>
2019-11-27 22:07:11 -0700

    Add border lines

--- assignment4/Main.py
+++ assignment4/Main.py
@@ -22,8 +22,8 @@ BAR_WIDTH = 25
 COLORS = [
     (230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200), (245, 130, 48), (145, 30, 180), (70, 240, 240),
     (240, 50, 230), (210, 245, 60), (250, 190, 190), (0, 128, 128), (230, 190, 255), (170, 110, 40), (255, 250, 200),
-    (128, 0, 0), (170, 255, 195), (128, 128, 0), (255, 215, 180), (0, 0, 128), (128, 128, 128), (255, 255, 255)
-]
+    (128, 0, 0), (170, 255, 195), (128, 128, 0), (255, 215, 180), (0, 0, 128), (128, 128, 128), (255, 255, 255)]
+SOURCE_COLOR = COLORS[1]
 
 
 # Draws a Sankey diagram of a given data set
@@ -39,35 +39,56 @@ def draw_sankey(data):
     # Draw source bar
     source_x = PADDING_X + SPACING_X
     source_y = (HEIGHT - total_flow * pixels_per_unit) / 2
-    setColor(*COLORS[0])
-    rect(source_x, source_y, BAR_WIDTH, total_flow * pixels_per_unit)
+    source_height = total_flow * pixels_per_unit
+    setColor(*SOURCE_COLOR)
+    rect(source_x, source_y, BAR_WIDTH, source_height)
 
     destination_x = WIDTH - PADDING_X
     destination_y = PADDING_Y
 
-    for k in data:
-        color = COLORS[list(data).index(k) + 1]
-        height = data[k] * pixels_per_unit
+    # Draw border around source bar
+    setColor("black")
+    line(source_x, source_y, source_x + BAR_WIDTH, source_y)
+    line(source_x, source_y, source_x, source_y + source_height)
+    line(source_x, source_y + source_height, source_x + BAR_WIDTH, source_y + source_height)
 
-        # Draw destination text
-        setColor("black")
-        text(destination_x + BAR_WIDTH + SPACING_X, destination_y + height / 2, k, "w")
+    for k in data:
+        color = COLORS[list(data).index(k) + list(COLORS).index(SOURCE_COLOR) + 1]
+        height = data[k] * pixels_per_unit
 
         # Draw destination bar
         setColor(*color)
         rect(destination_x, destination_y, BAR_WIDTH, height)
+
+        # Draw destination text
+        setColor("black")
+        text(destination_x + BAR_WIDTH + SPACING_X, destination_y + height / 2, k, "w")
 
         # Draw body
         offset_y = 0
         increment = (source_y - destination_y) / (destination_x - source_x - BAR_WIDTH)
         range_x = destination_x - source_x - BAR_WIDTH
         for x in range(range_x):
-            setColor(x / range_x * color[0],
-                     x / range_x * color[1],
-                     x / range_x * color[2])
+            setColor(SOURCE_COLOR[0] + (x / range_x) * (color[0] - SOURCE_COLOR[0]),
+                     SOURCE_COLOR[1] + (x / range_x) * (color[1] - SOURCE_COLOR[1]),
+                     SOURCE_COLOR[2] + (x / range_x) * (color[2] - SOURCE_COLOR[2]))
             line(source_x + BAR_WIDTH + x, source_y - offset_y,
                  source_x + BAR_WIDTH + x, source_y + height - offset_y)
+            # p = p * pi - pi / 2
+            # p = (sin(p) + 1) / 2
             offset_y += increment
+
+        setColor("black")
+        # Top border line of body
+        line(source_x + BAR_WIDTH, source_y, destination_x, destination_y)
+        # Bottom border line of body
+        line(source_x + BAR_WIDTH, source_y + height, destination_x, destination_y + height)
+        # Top destination border line
+        line(destination_x, destination_y, destination_x + BAR_WIDTH, destination_y)
+        # Right destination border line
+        line(destination_x + BAR_WIDTH, destination_y, destination_x + BAR_WIDTH, destination_y + height)
+        # Bottom destination border line
+        line(destination_x, destination_y + height, destination_x + BAR_WIDTH, destination_y + height)
 
         source_y += height
         destination_y += height + SPACING_Y
